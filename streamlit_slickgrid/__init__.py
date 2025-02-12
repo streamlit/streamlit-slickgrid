ded6557be64bd58a34b75657302147c1517c2612
Thiago Teixeira <thiago.teixeira@snowflake.com>
2025-02-11 22:55:54 -0800

    Add license headers

--- streamlit_slickgrid/__init__.py
+++ streamlit_slickgrid/__init__.py
@@ -1,20 +1,36 @@
+# Copyright 2025 Snowflake Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import streamlit as st
 import os
 import streamlit.components.v1 as components
 
-_RELEASE = False
-
+_RELEASE = True
 _NAME = "streamlit-slickgrid"
 
-if not _RELEASE:
+if _RELEASE:
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/build")
+    _component_func = components.declare_component(
+        _NAME,
+        path=build_dir,
+    )
+else:
     _component_func = components.declare_component(
         _NAME,
         url="http://localhost:3001",
     )
-else:
-    parent_dir = os.path.dirname(os.path.abspath(__file__))
-    build_dir = os.path.join(parent_dir, "frontend/build")
-    _component_func = components.declare_component(_NAME, path=build_dir)
 
 
 def slickgrid(data, columns, options=None, on_click=None, key=None):
